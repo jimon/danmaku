7f36eeba42db4afbdc588f12c21a166402c15544
Dmytro Ivanov <jimon.j1m0n@gmail.com>
2015-12-27 23:11:55 +0100

    characters basic

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
 -- danmaku sandbox
 -- bullet engine inspired by bullet pattern studio v1.8.0
 
-pprint = require("pprint")
+local pprint = require("pprint")
 pprint.setup {show_all = true, wrap_array = true}
 
-bullets = {}
+local bullets = {}
+local characters = {}
+local player = {x = 0, y = 300, chr = 1, t = 1, vx = 3, vy = 3}
 
 function emit_burst(x, y, t, num, v, vx, vy, start_radius)
 	vx = vx or 0
@@ -70,6 +72,7 @@ function love.load()
 
 	img_bg = love.graphics.newImage("bg.jpg")
 	img_bullets_atlas = love.graphics.newImage("bullets.png")
+	img_characters_atlas = love.graphics.newImage("characters.png")
 
 	img_bullets = {}
 	img_bullets[#img_bullets + 1] = love.graphics.newQuad(4 * 9 + 1, 1, 8, 8, img_bullets_atlas:getWidth(), img_bullets_atlas:getHeight())
@@ -77,14 +80,46 @@ function love.load()
 	img_bullets[#img_bullets + 1] = love.graphics.newQuad(0 * 10 + 681, 1, 9, 15, img_bullets_atlas:getWidth(), img_bullets_atlas:getHeight())
 	img_bullets[#img_bullets + 1] = love.graphics.newQuad(4 * 10 + 681, 1, 9, 15, img_bullets_atlas:getWidth(), img_bullets_atlas:getHeight())
 
+	img_characters = {}
+	img_characters[#img_characters + 1] = love.graphics.newQuad(0 * 64, 4 * 64, 64, 64, img_characters_atlas:getWidth(), img_characters_atlas:getHeight())
+	img_characters[#img_characters + 1] = love.graphics.newQuad(0 * 64, 3 * 64, 64, 64, img_characters_atlas:getWidth(), img_characters_atlas:getHeight())
+
 	emit_burst(0, 0, 4, 150, 5)--, 10, 10, 150)
 	emit_stream(0, 0, 2, 15, 5, 45)
+
+	characters[#characters + 1] = {x = 0, y = 300, t = 1}
+	characters[#characters + 1] = {x = 0, y = 0, t = 2}
 end
 
 function love.update(dt)
 	scrn_w = love.graphics.getWidth()
 	scrn_h = love.graphics.getHeight()
 
+	-- update player input
+	if player then
+		local pdx = 0
+		local pdy = 0
+		if love.keyboard.isDown("left") then  pdx = -1 end
+		if love.keyboard.isDown("right") then pdx =  1 end
+		if love.keyboard.isDown("up") then    pdy = -1 end
+		if love.keyboard.isDown("down") then  pdy =  1 end
+		if pdx * pdx + pdy * pdy > 1 then
+			pdx = pdx / math.sqrt(2)
+			pdy = pdy / math.sqrt(2)
+		end
+		player.x = player.x + player.vx * pdx
+		player.y = player.y + player.vy * pdy
+		img_x, img_y, img_w, img_h = img_bullets[player.t]:getViewport()
+		player.x = math.min(math.max(player.x, -scrn_w / 2 + img_w / 2), scrn_w / 2 - img_w / 2)
+		player.y = math.min(math.max(player.y, -scrn_h / 2 + img_h / 2), scrn_h / 2 - img_h / 2)
+		if player.chr then
+			characters[player.chr].x = player.x
+			characters[player.chr].y = player.y - 10
+		end
+	end
+
+	-- update bullets
+	delete_later = {}
 	for k, bullet in pairs(bullets) do
 		bullet.x = bullet.x + bullet.vx
 		bullet.y = bullet.y + bullet.vy
@@ -99,9 +134,18 @@ function love.update(dt)
 			(bullet.y + img_h / 2 < -scrn_h / 2) or
 			(bullet.y - img_h / 2 >  scrn_h / 2)
 			then
-			bullets[k] = nil
+			delete_later[k] = true
 		end
+	end
 
+	if #delete_later > 0 then
+		local new_bullets = {}
+		for k, bullet in pairs(bullets) do
+			if not delete_later[k] then
+				new_bullets[#new_bullets + 1] = bullet
+			end
+		end
+		bullets = new_bullets
 	end
 end
 
@@ -113,9 +157,20 @@ function love.draw()
 	love.graphics.draw(img_bg, 0, 0)
 
 	love.graphics.setColor(255, 255, 255)
+	-- render characters
+	for k, chr in pairs(characters) do
+		img_x, img_y, img_w, img_h = img_characters[chr.t]:getViewport()
+		love.graphics.draw(
+			img_characters_atlas,
+			img_characters[chr.t],
+			chr.x + scrn_w / 2 - img_w / 2,
+			chr.y + scrn_h / 2 - img_h / 2,
+			chr.a)
+	end
+
+	-- render bullets
 	for k, bullet in pairs(bullets) do
 		img_x, img_y, img_w, img_h = img_bullets[bullet.t]:getViewport()
-
 		love.graphics.draw(
 			img_bullets_atlas,
 			img_bullets[bullet.t],
@@ -124,7 +179,17 @@ function love.draw()
 			bullet.a - math.pi / 2.0)
 	end
 
-	love.graphics.print("bullets " .. #bullets, 0, 0)
+	-- render player
+	if player then
+		img_x, img_y, img_w, img_h = img_bullets[player.t]:getViewport()
+		love.graphics.draw(
+			img_bullets_atlas,
+			img_bullets[player.t],
+			player.x + scrn_w / 2 - img_w / 2,
+			player.y + scrn_h / 2 - img_h / 2)
+	end
+
+	love.graphics.print("bullets " .. #bullets, 2, 2)
 end
 
 function love.keypressed(key)
